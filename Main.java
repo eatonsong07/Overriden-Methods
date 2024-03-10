a08813773682449ad75be82a91a8ab56acaf055b
eatonsong07 <144486854+eatonsong07@users.noreply.github.com>
2024-03-10 11:01:53 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Student eaton= new Student("eaton", 17, 11, "Brooklyn Tech" );
+        System.out.println(eaton.getInformation());
+
+    }
+}
